074cec8783dc52412a6dff32aaea5ba1b3adecb0
akhilkrishnair <akhilkrishna36912@gmail.com>
2023-08-22 08:51:42 +0530

    doctor and department page created

--- healthcare_project/Home/urls.py
+++ healthcare_project/Home/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('',views.index, name='home'),
     path('about',views.about, name='about'),
     path('enquery',views.enquery, name='enquery'),
+    path('appointment',views.appointment, name='appointment'),
+    path('doctors',views.doctors, name='doctors'),
+    path('departments',views.departments, name='departments'),
 ]
--- healthcare_project/Home/views.py
+++ healthcare_project/Home/views.py
@@ -12,3 +12,17 @@ def about(request):
 
 def enquery(request):
     return render(request, 'enquery.html')
+
+
+
+
+def appointment(request):
+    return render(request, 'appointment.html')
+
+
+def doctors(request):
+    return render(request, 'doctors.html')
+
+
+def departments(request):
+    return render(request, 'departments.html')
